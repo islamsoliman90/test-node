e81247d1e251884b325f187dc9c7e133fe1580d1
islamsoliman90 <islam.soliman1990@gmail.com>
2023-09-06 07:43:09 +0300

    final lesson 7

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ const app = express();
 app.listen(3000);
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
-app.get("/about-me", (req, res) => {
-  res.redirect("./views/about.html", { root: __dirname });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create blog" });
 });
-
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("/404", { title: "404" });
 });
