b03c8b954b8417c5ea2a908a830e05fabffca0bb
islamsoliman90 <islam.soliman1990@gmail.com>
2023-09-12 21:21:23 +0300

    add and delete

--- app.js
+++ app.js
@@ -17,12 +17,16 @@ mongoose
   });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+app.get("/blogs", (req, res) => {
   Blogs.find()
     .sort({ createdAt: -1 })
     .then((blogs) => {
@@ -31,14 +35,10 @@ app.get("/", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-app.get("/add-blogs", (req, res) => {
-  const blog = new Blogs({
-    title: "new blog",
-    snippet: "about my new blog",
-    body: "more about my new blog",
-  });
+app.post("/blogs/add-blogs", (req, res) => {
+  const blog = new Blogs(req.body);
   blog.save().then((result) => {
-    res.render("index.ejs", { title: "blogs", blogs: result });
+    res.redirect("/blogs");
   });
 });
 
@@ -47,6 +47,22 @@ app.get("/about", (req, res) => {
 });
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "create blog" });
+});
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blogs.findById(id)
+    .then((blog) => {
+      res.render("details.ejs", { title: "blog details", blog });
+    })
+    .catch((err) => console.log(err));
+});
+app.delete("/blogs/del-blog/:id", (req, res) => {
+  const id = req.params.id;
+  Blogs.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => console.log(err));
 });
 app.use((req, res) => {
   res.status(404).render("/404", { title: "404" });
